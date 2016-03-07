1a251da375c425b80c0210f113a74550683841fb
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2016-03-07 16:57:16 +0100

    Added files required to run HAT-P-11b retrieval.

---
+++ 021_Figure09_HAT-P-11b_spectra-retrieval.py
@@ -0,0 +1,246 @@
+import sys, os, time
+from scipy.ndimage.filters import gaussian_filter1d as gaussf
+
+sys.path.append("./BART/modules/transit/transit/python")
+import transit_module as tm
+sys.path.append("./BART/code")
+import makeatm as ma
+import wine as w
+import bestFit as bf
+
+sys.path.append("/home/patricio/ast/esp01/bart/main/hat-p-11b_transit_2015-05-07/runtransit3")
+import balance as b
+
+sys.path.append("/home/patricio/ast/esp01/code/lib/c/pt/lib")
+import pt as pt
+
+# HST data:
+wlength = np.array([
+  1.153, 1.172, 1.190, 1.209, 1.228, 1.247, 1.266, 1.284, 1.303, 1.322,
+  1.341, 1.360, 1.379, 1.397, 1.416, 1.435, 1.454, 1.473, 1.492, 1.510,
+  1.529, 1.548, 1.567, 1.586, 1.604, 1.623, 1.642, 1.661, 1.680])
+depth = 1e-6 * np.array([
+  3502, 3407, 3421, 3445, 3350, 3377, 3380, 3457, 3436, 3448, 3476,
+  3536, 3499, 3498, 3524, 3591, 3524, 3520, 3447, 3344, 3513, 3471,
+  3438, 3414, 3383, 3415, 3480, 3498, 3376.])
+uncert = 1e-6 * np.array([40, 47, 46, 38, 41, 38, 45, 40, 35, 39, 43, 44, 46,
+              40, 41, 44, 43, 39, 39, 45, 41, 50, 49, 53, 45, 38, 48, 60, 74.])
+
+# Spitzer data:
+swlength = np.array([3.6, 4.5])
+sdepth  = np.array([0.003354, 0.003382])
+suncert = np.array([0.000025, 0.000029])
+
+cfgfile = "run07_HAT-P-11b_BART/BARToffset/bestFit_tconfig.cfg"
+args = ["transit", "-c", cfgfile]
+tm.transit_init(len(args), args)
+
+# Spitzer Filters:
+irac1_wn, irac1_tr = w.readfilter("BART/inputs/filters/spitzer_irac1_sa.dat")
+irac2_wn, irac2_tr = w.readfilter("BART/inputs/filters/spitzer_irac2_sa.dat")
+irac1_tr -= np.amin(irac1_tr)
+irac2_tr -= np.amin(irac2_tr)
+irac1_tr /= np.amax(irac1_tr)
+irac2_tr /= np.amax(irac2_tr)
+irac1_wl = 1e4/irac1_wn
+irac2_wl = 1e4/irac2_wn
+
+# Best-fit:
+mol, press, temp, abund = ma.readatm("/home/patricio/ast/esp01/bart/compendium/PhD_dissertation_Cubillos_UCF/run07_HAT-P-11b_BART/BARToffset/bestFit.atm")
+# Initial guess:
+mol2, press2, temp2, abund2 = ma.readatm("run07_HAT-P-11b_BART/BARTinputs/atmosphe_HAT-P-11b.atm")
+
+# Temperature posterior:
+sample = np.load("run07_HAT-P-11b_BART/BARToffset/output.npy")
+burn=1000
+p0 = sample[:9,0,burn:].flatten()
+p1 = sample[:9,1,burn:].flatten()
+p2 = sample[:9,2,burn:].flatten()
+
+tb = np.zeros(len(press))  # Best
+bestpars  = np.array([-3.7916148, -6.0099733e-01,  1.0,  0.0, 5.3374064e-01])
+
+Rs, Ts, sma, gstar = bf.get_starData("inputs/TEP/HAT-P-11b.tep")
+grav, Rp = ma.get_g("inputs/TEP/HAT-P-11b.tep")
+pt.pt(bestpars,  press, tb, Rs, Ts, 100.0, sma, grav*100)
+
+tprofiles = np.zeros((np.size(p0), len(press)))
+Tparams = np.array([-2.8,  -0.55, 1.0, 0.0, 0.965])
+
+for i in np.arange(np.size(p0)):
+  Tparams[0] = p0[i]
+  Tparams[1] = p1[i]
+  Tparams[4] = p2[i]
+  t = pt.pt(Tparams, press, tprofiles[i], Rs, Ts, 100, sma, grav*100)
+  if i %1000 == 0:
+    print(i)
+
+low1 = np.percentile(tprofiles, 16,   axis=0)
+hi1  = np.percentile(tprofiles, 84,   axis=0)
+low2 = np.percentile(tprofiles,  2.5, axis=0)
+hi2  = np.percentile(tprofiles, 97.5, axis=0)
+median = np.median(tprofiles, axis=0)
+
+# Radius posterior at 0.1 bar:
+rad     = sample[:9,3,burn:].flatten()
+# WFC3 offset (ppm):
+poffset = sample[:9,4,burn:].flatten() * 1e6
+# abundances at 0.1 bar:
+ipress = 44
+pH2O    = sample[:9,5,burn:].flatten() + np.log10(abund2[ipress][9])
+pCH4    = sample[:9,6,burn:].flatten() + np.log10(abund2[ipress][8])
+pCO     = sample[:9,7,burn:].flatten() + np.log10(abund2[ipress][6])
+pCO2    = sample[:9,8,burn:].flatten() + np.log10(abund2[ipress][7])
+
+# Initialize stuff:
+imol = [1,5]
+na = np.copy(abund)
+rat, irat = b.ratio(na, imol)
+profiles = np.vstack((temp, abund.T))
+wn = tm.get_waveno_arr(tm.get_no_samples())
+wl = 1e4/wn
+
+# Best:
+tm.set_radius(2.9751187e+04)   # SET ME!
+na = np.copy(abund2)
+rat, irat = b.ratio(na, imol)
+na[:, 9] *= 10**2.1036386e+00   # SET ME!
+na[:, 8] *= 10**2.7242965e+00
+na[:, 6] *= 10**1.6548319e+00
+na[:, 7] *= 10**-2.1056120e+00
+b.balance(na, imol, rat, irat)
+profiles = np.vstack((temp, na.T))
+tmp2 = tm.run_transit(profiles.flatten(), tm.get_no_samples())
+
+tm.set_radius(29600.0)  # Solar
+na = np.copy(abund2)
+rat, irat = b.ratio(na, imol)
+b.balance(na, imol, rat, irat)
+profiles = np.vstack((temp2, na.T))
+tmp3 = tm.run_transit(profiles.flatten(), tm.get_no_samples())
+
+tcold = temp2*0.4
+tm.set_radius(29900.0)  # Cold
+na = np.copy(abund2)
+rat, irat = b.ratio(na, imol)
+na[:, 9] *= 10**0.0   # H2O
+na[:, 8] *= 10**0.0   # CH4
+na[:, 6] *= 10** 0.0   # CO
+na[:, 7] *= 10** 0.0   # CO2
+b.balance(na, imol, rat, irat)
+profiles = np.vstack((tcold, na.T))
+tmp4 = tm.run_transit(profiles.flatten(), tm.get_no_samples())
+
+fs = 12
+lw = 1.5
+yran = 0.0031, 0.00385
+
+sigma = 6
+mod2 = gaussf(tmp2, sigma)  # Best
+mod3 = gaussf(tmp3, sigma)  # Solar
+mod4 = gaussf(tmp4, sigma)
+
+offset = 1.2142876e-04   # SET ME!
+plt.figure(4, (8.5,3.5))
+plt.clf()
+plt.subplots_adjust(0.13, 0.15, 0.95, 0.95)
+ax = plt.subplot(111)
+plt.semilogx(wl, mod3, "darkorange", lw=lw, label=r"${\rm Solar}$")
+plt.semilogx(wl, mod4, "limegreen",  lw=lw, label=r"${\rm Low\ temp.}$")
+plt.semilogx(wl, mod2, "b",          lw=lw, label=r"${\rm Best\ fit}$")
+plt.errorbar(wlength,  depth-offset, uncert, fmt="or", ms=5, elinewidth=lw,
+             zorder=3, capthick=lw)
+plt.errorbar(swlength, sdepth,      suncert, fmt="or", ms=5, elinewidth=lw,
+             zorder=3, capthick=lw, label=r"${\rm Data}$")
+plt.plot(irac1_wl, 0.0001*irac1_tr + yran[0], color="0.5", lw=lw)
+plt.plot(irac2_wl, 0.0001*irac2_tr + yran[0], color="0.5", lw=lw)
+leg = plt.legend(loc="upper left", fontsize=fs-1)
+leg.get_frame().set_alpha(0.5)
+plt.xlabel(r"${\rm Wavelength\ \ (um)}$", fontsize=fs)
+plt.ylabel(r"$(R_p/R_s)^2$",              fontsize=fs)
+ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+ax.set_xticks([1, 2, 3, 4, 5])
+plt.xlim(1.0, 5.5)
+plt.ylim(yran)
+plt.savefig("plots/HAT-P-11b_spectra.ps")
+
+fs=11
+lw=1.5
+mname = ["", r"${\rm He}$", "", "", "", r"${\rm H}_2$",
+       r"${\rm CO}$", r"${\rm CO}_2$", r"${\rm CH}_4$", r"${\rm H}_2{\rm O}$",
+       r"${\rm NH}_3$", r"${\rm C}_2{\rm H}_2$", "", r"${\rm HCN}$"]
+matplotlib.rcParams.update({'xtick.minor.size':0.0})
+matplotlib.rcParams.update({'ytick.minor.size':0.0})
+plt.figure(5, (8.5,3.5))
+plt.clf()
+ax = plt.axes([0.08, 0.18, 0.3, 0.75])
+plt.loglog(abund2[:, 5], press, "-",  lw=lw, label=mname[ 5], color="blue")
+plt.loglog(abund2[:, 1], press, "-",  lw=lw, label=mname[ 1],color="darkorange")
+plt.loglog(abund2[:, 6], press, "-",  lw=lw, label=mname[ 6], color="limegreen")
+plt.loglog(abund2[:, 7], press, "-",  lw=lw, label=mname[ 7], color="r")
+plt.loglog(abund2[:, 8], press, "-",  lw=lw, label=mname[ 8], color="m")
+plt.loglog(abund2[:, 9], press, "-",  lw=lw, label=mname[ 9], color="k")
+plt.loglog(abund2[:,10], press, "--", lw=lw, label=mname[10], color="brown")
+plt.loglog(abund2[:,11], press, "--", lw=lw, label=mname[11], color="c")
+plt.loglog(abund2[:,13], press, "--", lw=lw, label=mname[13], color="0.4")
+plt.legend(loc="lower left", fontsize=fs-1)
+plt.ylim(press[0], press[-1])
+plt.xlim(1e-26, 1e1)
+ax.set_xticks([1e-25, 1e-20, 1e-15, 1e-10, 1e-5, 1e-0])
+ax.set_yticks([1e-8, 1e-6, 1e-4, 1e-2, 1e-0, 1e2])
+plt.xlabel(r"${\rm Mole\ mixing\ ratio}$",     fontsize=fs)
+plt.ylabel(r"${\rm Pressure\ \ (bar)}$", fontsize=fs)
+matplotlib.rcParams.update({'xtick.minor.size':3.5})
+
+ax = plt.axes([0.4, 0.18, 0.17, 0.75])
+ax.fill_betweenx(press, low2, hi2, facecolor="#62B1FF", edgecolor="0.5")
+ax.fill_betweenx(press, low1, hi1, facecolor="#1873CC", edgecolor="#1873CC")
+plt.semilogy(median, press, "b-", lw=1.5, label=r'$\rm Median$')
+plt.semilogy(tb,     press,  "-", lw=1.5, label=r"$\rm Best\ fit$", color="r")
+plt.ylim(press[0], press[-1])
+ax.set_xticks([0, 600, 1200, 1800, 2400])
+plt.xlim(0, 2000)
+axl = plt.legend(loc="upper right", fontsize=fs-1)
+axl.get_frame().set_alpha(0.6)
+ax.set_yticklabels([""])
+ax.set_yticks([1e-8, 1e-6, 1e-4, 1e-2, 1e-0, 1e2])
+plt.xlabel(r"${\rm Temperature\ \ (K)}$", fontsize=fs)
+
+nb = 14
+lhx = 0.59
+dhx = 0.12
+dhy = 0.3
+ax = plt.axes([lhx,            0.33 + dhy, dhx, dhy]) # TOP LEFT
+ax.set_yticklabels([""])
+h, hx, patch = plt.hist(rad, bins=nb)
+ax.set_xticks([29000, 30000])
+plt.xlabel(r"${\rm Radius\ (km)}$", fontsize=fs)
+ax = plt.axes([lhx,            0.18,       dhx, dhy]) # BOT LEFT
+ax.set_yticklabels([""])
+h, hx, patch = plt.hist(poffset, bins=nb)
+ax.set_xticks([0,100, 200])
+plt.xlabel(r"${\rm WFC3\ offset\ (ppm)}$", fontsize=fs)
+ax = plt.axes([lhx+0.02+  dhx, 0.33 + dhy, dhx, dhy]) # TOP CEN
+ax.set_yticklabels([""])
+h, hx, patch = plt.hist(pH2O, bins=nb)
+ax.set_xticks([-3, -2, -1, 0])
+plt.xlabel(r"$\log_{10}({\rm H2O})$", fontsize=fs)
+ax = plt.axes([lhx+0.02+  dhx, 0.18,       dhx, dhy]) # BOT CEN
+ax.set_yticklabels([""])
+h, hx, patch = plt.hist(pCH4, bins=nb)
+plt.xlim(hx[0], 0)
+ax.set_xticks([-10, -5, 0])
+plt.xlabel(r"$\log_{10}({\rm CH4})$", fontsize=fs)
+ax = plt.axes([lhx+0.04+2*dhx, 0.33 + dhy, dhx, dhy]) # TOP RIGHT
+ax.set_yticklabels([""])
+h, hx, patch = plt.hist(pCO, bins=nb)
+ax.set_xticks([-10, -5, 0])
+plt.xlabel(r"$\log_{10}({\rm CO})$", fontsize=fs)
+ax = plt.axes([lhx+0.04+2*dhx, 0.18,       dhx, dhy]) # BOT RIGHT
+ax.set_yticklabels([""])
+ax.set_xticks([-15, -10, -5, 0])
+h, hx, patch = plt.hist(pCO2, bins=nb)
+ax.set_xlim(hx[0], 0)
+plt.xlabel(r"$\log_{10}({\rm CO2})$", fontsize=fs)
+
+plt.savefig("plots/HAT-P-11b_atmosphere.ps")
---
+++ inputs/ancil/bestFit.py
@@ -0,0 +1,561 @@
+# Copyright (C) 2015-2016 University of Central Florida. All rights reserved.
+# BART is under an open-source, reproducible-research license (see LICENSE).
+
+"""
+    This code runs and processes best-fit Transit run outputs.
+    
+    Functions
+    ---------
+    read_MCMC_out:
+          Read the MCMC output log file. Extract the best fitting parameters.
+    get_params:
+	      Get correct number of all parameters from stepsize
+    get_starData:
+          Extract stellar temperature, radius, and mass from TEP file
+    write_atmfile:
+          Write best-fit atm file with scaled H2 and He to abundances sum of 1
+    bestFit_tconfig:
+          Write best-fit config file for best-fit Transit run
+    callTransit:
+          Call Transit to produce best-fit outputs. Plot MCMC posterior PT plot.
+    plot_bestFit_Spectrum:
+          Plot BART best-model spectrum
+
+    Revisions
+    ---------
+    2015-05-03  Jasmina  Original implementation
+    2015-07-12  Jasmina  Added documentation.
+"""
+
+import numpy as np
+import reader as rd
+import scipy.constants as sc
+import scipy.special   as sp
+import scipy.interpolate as si
+from scipy.ndimage.filters import gaussian_filter1d as gaussf
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import makeatm as mat
+import PT as pt
+import wine as w
+import readtransit as rt
+import constants as c
+
+def read_MCMC_out(MCfile):
+    """
+    Read the MCMC output log file. Extract the best fitting parameters.
+    """
+    # Open file to read
+    f = open(MCfile, 'r')
+    lines = np.asarray(f.readlines())
+    f.close() 
+
+    # Find where the data starts and ends:
+    for ini in np.arange(len(lines)):
+        if lines[ini].startswith(' Best-fit params'):
+            break
+    ini += 1
+    end = ini
+    for end in np.arange(ini, len(lines)):
+        if lines[end].strip() == "":
+            break
+
+    # Read data:
+    bestP = np.zeros(end-ini+1, np.double)
+    uncer = np.zeros(end-ini+1, np.double)
+    for i in np.arange(ini, end):
+        parvalues = lines[i].split()
+        bestP[i-ini] = parvalues[0]
+        uncer[i-ini] = parvalues[1]
+
+    return bestP, uncer
+
+
+def get_params(bestP, stepsize, params):
+    """
+    Get correct number of all parameters from stepsize
+    """
+    j = 0
+    allParams = np.zeros(len(stepsize))
+    for i in np.arange(len(stepsize)):
+        if stepsize[i] != 0.0:
+            allParams[i] = bestP[j]
+            j +=1
+        else:
+            allParams[i] = params[i]
+            
+    return allParams
+
+
+def get_starData(tepfile):
+    """
+    Extract the Stellar temperature, radius, and mass from a TEP file.
+    """
+    # Open tepfile to read and get data:
+    tep = rd.File(tepfile)
+
+    # Get star mass in Mjup:
+    Tstar = np.float(tep.getvalue('Ts')[0])
+
+    # Get star radius in MKS units:
+    Rstar = np.float(tep.getvalue('Rs')[0]) * c.Rsun
+
+    # Get semi major axis in meters:
+    sma = np.float(tep.getvalue('a')[0]) * sc.au
+
+    # Get star loggstar:
+    gstar = np.float(tep.getvalue('loggstar')[0])
+
+    return Rstar, Tstar, sma, gstar
+
+
+def write_atmfile(atmfile, molfit, rad, T_line, allParams, date_dir):
+    """
+    Write best-fit atm file with scaled H2 and He to abundances sum of 1.
+
+    Parameters:
+    -----------
+    atmfile: String
+      Atmospheric file to take the pressure and abundance profiles.
+    molfit: 1D string ndarray
+      List of molecule names to modify their abundances.
+    rad: 1D float ndarray
+      Modified radius of the atmospheric layers.
+    T_line: 1D float ndarray
+      Modified temperature of the atmospheric layers.
+    allParams: 1D float ndarray
+      List of scaling factors to modify the abundances of molfit molecules.
+    date_dir: String
+      Directory where to store the best-fit atmospheric file.
+    """
+    # Open atmfile to read
+    f = open(atmfile, 'r')
+    lines = np.asarray(f.readlines())
+    f.close()
+
+    # Get the molecules
+    imol = np.where(lines == "#SPECIES\n")[0][0] + 1
+    molecules = lines[imol].split()
+
+    # Find the line where the layers info begins
+    start = np.where(lines == "#TEADATA\n")[0][0] + 2 
+    headers = lines[start-1].split()
+    datalines = lines[start:]
+
+    # Number of columns
+    ncol = len(lines[start].split())
+
+    # Number of layers
+    ndata = len(datalines)  
+
+    # Allocate space for pressure:
+    pressure = np.zeros(ndata, np.double) 
+
+    # Number of abundances (elements per line except Radius, Press and T) 
+    nabun = len(lines[start].split()) - 3  
+
+    data = np.zeros((ndata, len(headers)))
+    for i in np.arange(ndata):
+        data[i] = datalines[i].split()
+
+    # Extract pressure data:
+    pressure = data[:,1]
+
+    # Fill out the abundances array:
+    abundances = np.zeros((len(molecules), ndata))
+    for i in np.arange(len(molecules)):
+        for j in np.arange(ndata):
+            abundances[i] = data[:, i+3]
+
+    # recognize which columns to take from the atmospheric file
+    headers = lines[start-1].split()
+    columns = np.zeros(len(molfit))
+    for i in np.arange(len(molfit)):
+        for j in np.arange(len(headers)):
+            if molfit[i] == headers[j]:
+                columns[i] = j
+
+    # number of molecules to fit:
+    nfit = len(molfit)
+    abun_fact = allParams
+
+    # multiply the abundances of molfit molecules
+    for i in np.arange(len(columns)):
+       abundances[columns[i]-3] = abundances[columns[i]-3] * 10**abun_fact[i]
+
+    # ===== Scale H2 and He if sum abundances > 1 ===== #
+    # Find index for Hydrogen and Helium
+    molecules = np.asarray(molecules)
+    iH2     = np.where(molecules=="H2")[0][0]
+    iHe     = np.where(molecules=="He")[0][0]
+
+    # Get H2/He abundance ratio:
+    ratio = (abundances[iH2,:] / abundances[iHe,:])
+
+    # Find which level has sum(abundances)>1 and get the difference
+    q = np.sum(abundances, axis=0) - 1
+
+    # Correct H2, and He abundances conserving their ratio:
+    for i in np.arange(ndata):
+    #    if q[i]>0:
+            abundances[iH2, i] -= ratio[i] * q[i] / (1.0 + ratio[i])
+            abundances[iHe, i] -=            q[i] / (1.0 + ratio[i])
+
+    # open best fit atmospheric file
+    fout = open(date_dir + 'bestFit.atm', 'w')
+    fout.writelines(lines[:start])
+
+    # Write atm file for each run
+    for i in np.arange(ndata): 
+        # Radius, pressure, and temp for the current line
+        radi = str('%10.3f'%rad[i])
+        presi = str('%10.4e'%pressure[i])
+        tempi = str('%7.2f'%T_line[i])
+
+        # Insert radii array
+        fout.write(radi.ljust(10) + ' ')
+
+        # Insert results from the current line (T-P) to atm file
+        fout.write(presi.ljust(10) + ' ')
+        fout.write(tempi.ljust(7) + ' ')
+
+        # Write current abundances
+        for j in np.arange(len(headers) - 3):
+            fout.write('%1.4e'%abundances[j][i] + ' ')
+        fout.write('\n')
+
+    # Close atm file
+    fout.close()
+
+
+def bestFit_tconfig(tconfig, date_dir):
+  '''
+  Write best-fit config file for best-fit Transit run
+  '''
+  # Open atmfile to read
+  f = open(date_dir + tconfig, 'r')
+  lines = np.asarray(f.readlines())
+  f.close()
+
+  # Change name to the atmfile in line zero
+  atm_line = 'atm ' + date_dir + 'bestFit.atm' + '\n'
+  lines[0] = atm_line
+
+  # Write lines into the bestFit config file
+  f = open(date_dir + 'bestFit_tconfig.cfg', 'w')
+  f.writelines(lines)
+  #f.writelines('savefiles yes')
+  f.close()
+
+
+def callTransit(atmfile, tepfile, MCfile, stepsize, molfit, solution,
+                p0, tconfig, date_dir, params, burnin, abun_file):
+    """
+    Call Transit to produce best-fit outputs.
+    Plot MCMC posterior PT plot.
+
+    Parameters:
+    -----------
+    atmfile: String
+       Atmospheric file.
+    tepfile: String
+       Transiting extra-solar planet file.
+    MCfile: String
+       File with MCMC log and best-fitting results.
+    stepsize: 1D float ndarray
+       FINDME
+    molfit: 1D String ndarray
+       List of molecule names to modify their abundances.
+    solution: String
+       Flag to indicate transit or eclipse geometry
+    p0: Float
+       Atmosphere's 'surface' pressure level.
+    tconfig: String
+       Transit  configuration file.
+    date_dir: String
+       Directory where to store results.
+    params: 1D float ndarray
+    burnin: Integer
+    abun_file: String
+       Elemental abundances file.
+    """
+
+    # read atmfile
+    molecules, pressure, temp, abundances = mat.readatm(atmfile)
+
+    # get surface gravity
+    grav, Rp = mat.get_g(tepfile)
+
+    # get star data
+    R_star, T_star, sma, gstar = get_starData(tepfile)
+
+    # Get best parameters
+    bestP, uncer = read_MCMC_out(MCfile)
+
+    # get all params
+    #allParams = get_params(bestP, stepsize, params)
+    allParams = bestP
+
+    # get PTparams and abundances factors
+    nparams = len(allParams)
+    nmol = len(molfit)
+    nradfit = int(solution == 'transit')
+    # EDIT:
+    noffset = 1
+    nPTparams = nparams - nmol - nradfit - noffset
+
+    PTparams  = allParams[:nPTparams]
+
+    # FINDME: Hardcoded value:
+    T_int = 100  # K
+
+    # call PT line profile to calculate temperature
+    best_T = pt.PT_line(pressure, PTparams, R_star, T_star, T_int,
+                        sma, grav*1e2)
+
+    # Plot best PT profile
+    plt.figure(1)
+    plt.clf()
+    plt.semilogy(best_T, pressure, '-', color = 'r')
+    plt.xlim(0.9*min(best_T), 1.1*max(best_T))
+    plt.ylim(max(pressure), min(pressure))
+    plt.title('Best PT', fontsize=14)
+    plt.xlabel('T [K]'     , fontsize=14)
+    plt.ylabel('logP [bar]', fontsize=14)
+    # Save plot to current directory
+    plt.savefig(date_dir + 'Best_PT.png') 
+
+    # Update R0, if needed:
+    if nradfit:
+      Rp = allParams[nPTparams]
+    # Mean molecular mass:
+    mu  = mat.mean_molar_mass(abun_file, atmfile)
+    # Re-calculate the layers' radii using the Hydrostatic-equilibrium calc:
+    # (Has to be in reversed order since the interpolation requires the
+    #  pressure array in increasing order)
+    rad = mat.radpress(pressure[::-1], best_T[::-1], mu[::-1], p0, Rp, grav)
+    rad = rad[::-1]
+
+    # write best-fit atmospheric file
+    # EDIT:
+    write_atmfile(atmfile, molfit, rad, best_T,
+                  allParams[nPTparams+nradfit+noffset:], date_dir)
+
+    # bestFit atm file
+    bestFit_atm = date_dir + 'bestFit.atm'
+
+    # write new bestFit Transit config
+    bestFit_tconfig(tconfig, date_dir)
+
+    # ========== plot MCMC PT profiles ==========
+
+    # get MCMC data:
+    MCMCdata = date_dir + "/output.npy"
+    data = np.load(MCMCdata)
+    nchains, npars, niter = np.shape(data)
+
+    # stuck chains:
+    data_stack = data[0,:,burnin:]
+    for c in np.arange(1, nchains):
+        data_stack = np.hstack((data_stack, data[c, :, burnin:]))
+
+    # create array of PT profiles
+    PTprofiles = np.zeros((np.shape(data_stack)[1], len(pressure)))
+
+    # current PT parameters for each chain, iteration
+    curr_PTparams = PTparams
+
+    # fill-in PT profiles array
+    print("  Plotting MCMC PT profile figure.")
+    for k in np.arange(0, np.shape(data_stack)[1]):
+        j = 0
+        for i in np.arange(len(PTparams)):
+            if stepsize[i] != 0.0:
+                curr_PTparams[i] = data_stack[j,k]
+                j +=1
+            else:
+                pass
+        PTprofiles[k] = pt.PT_line(pressure, curr_PTparams, R_star, T_star,
+                                   T_int, sma, grav*1e2)
+
+    # get percentiles (for 1,2-sigma boundaries):
+    low1 = np.percentile(PTprofiles, 16.0, axis=0)
+    hi1  = np.percentile(PTprofiles, 84.0, axis=0)
+    low2 = np.percentile(PTprofiles,  2.5, axis=0)
+    hi2  = np.percentile(PTprofiles, 97.5, axis=0)
+    median = np.median(PTprofiles, axis=0)
+
+    # plot figure
+    plt.figure(2)
+    plt.clf()
+    ax=plt.subplot(111)
+    ax.fill_betweenx(pressure, low2, hi2, facecolor="#62B1FF", edgecolor="0.5")
+    ax.fill_betweenx(pressure, low1, hi1, facecolor="#1873CC",
+                                                           edgecolor="#1873CC")
+    plt.semilogy(median, pressure, "-", lw=2, label='Median',color="k")
+    plt.semilogy(best_T, pressure, "-", lw=2, label="Best fit", color="r")
+    plt.ylim(pressure[0], pressure[-1])
+    plt.legend(loc="best")
+    plt.xlabel("Temperature  (K)", size=15)
+    plt.ylabel("Pressure  (bar)",  size=15)
+
+    # save figure
+    savefile = date_dir + "MCMC_PTprofiles.png" 
+    plt.savefig(savefile)
+
+
+
+def plot_bestFit_Spectrum(filters, kurucz, tepfile, solution, output, data,
+                                                          uncert, date_dir):
+    '''
+    Plot BART best-model spectrum
+    '''
+    # get star data
+    R_star, T_star, sma, gstar = get_starData(tepfile)
+
+    # get surface gravity
+    grav, Rp = mat.get_g(tepfile)
+
+    # convert Rp to m
+    Rp = Rp * 1000
+
+    # ratio planet to star
+    rprs = Rp/R_star
+  
+    # read kurucz file
+    starfl, starwn, tmodel, gmodel = w.readkurucz(kurucz, T_star, gstar)
+
+    # read best-fit spectrum output file, take wn and spectra values
+    if solution == 'eclipse':
+        specwn, bestspectrum = rt.readspectrum(date_dir + output, wn=True)
+        # print on screen
+        print("  Plotting BART best-fit eclipse spectrum figure.")
+    elif solution == 'transit':
+        specwn, bestspectrum = rt.readspectrum(date_dir + output, wn=True)
+        # print on screen
+        print("  Plotting BART best-fit modulation spectrum figure.")
+
+    # convert wn to wl
+    specwl = 1e4/specwn
+
+    # number of filters
+    nfilters = len(filters)
+
+    # read and resample the filters:
+    nifilter  = [] # Normalized interpolated filter
+    istarfl   = [] # interpolated stellar flux
+    wnindices = [] # wavenumber indices used in interpolation
+    meanwn    = [] # Filter mean wavenumber
+    for i in np.arange(nfilters):
+        # read filter:
+        filtwaven, filttransm = w.readfilter(filters[i])
+        meanwn.append(np.sum(filtwaven*filttransm)/sum(filttransm))
+        # resample filter and stellar spectrum:
+        nifilt, strfl, wnind = w.resample(specwn, filtwaven, filttransm,
+                                            starwn,    starfl)
+        nifilter.append(nifilt)
+        istarfl.append(strfl)
+        wnindices.append(wnind)
+
+    # convert mean wn to mean wl
+    meanwl = 1e4/np.asarray(meanwn)
+
+    # band-integrate the flux-ratio or modulation:
+    bandflux = np.zeros(nfilters, dtype='d')
+    bandmod  = np.zeros(nfilters, dtype='d')
+    for i in np.arange(nfilters):
+        fluxrat = (bestspectrum[wnindices[i]]/istarfl[i]) * rprs*rprs
+        bandflux[i] = w.bandintegrate(fluxrat, specwn, nifilter[i],
+                                                                 wnindices[i])
+        bandmod[i]  = w.bandintegrate(bestspectrum[wnindices[i]],
+                                            specwn, nifilter[i], wnindices[i])
+
+    # stellar spectrum on specwn:
+    sinterp = si.interp1d(starwn, starfl)
+    sflux = sinterp(specwn)
+    frat = bestspectrum/sflux * rprs * rprs
+
+    # plot figure
+    plt.rcParams["mathtext.default"] = 'rm'
+    matplotlib.rcParams.update({'mathtext.default':'rm'})
+    matplotlib.rcParams.update({'font.size':10})
+    plt.figure(3, (8.5, 5))
+    plt.clf()
+
+    # depending on solution plot eclipse or modulation spectrum
+    if solution == 'eclipse':
+        gfrat = gaussf(frat, 2)
+        plt.semilogx(specwl, gfrat*1e3, "b", lw=1.5, label="Best-fit")
+        plt.errorbar(meanwl, data*1e3, uncert*1e3, fmt="or", label="data")
+        plt.plot(meanwl, bandflux*1e3, "ok", label="model", alpha=1.0)
+        plt.ylabel(r"$F_p/F_s$ (10$^{3}$)", fontsize=12)
+
+    elif solution == 'transit':
+        gmodel = gaussf(bestspectrum, 2)
+        plt.semilogx(specwl, gmodel, "b", lw=1.5, label="Best-fit")
+        # Check units!
+        plt.errorbar(meanwl, data, uncert, fmt="or", label="data")
+        plt.plot(meanwl, bandmod, "ok", label="model", alpha=0.5)
+        plt.ylabel(r"$(R_p/R_s)^2$", fontsize=12)
+
+    leg = plt.legend(loc="lower right")
+    leg.get_frame().set_alpha(0.5)
+    ax = plt.subplot(111)
+    ax.set_xscale('log')
+    plt.xlabel(r"${\rm Wavelength\ \ (um)}$", fontsize=12)  
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_xticks(np.arange(round(min(specwl)),max(specwl),1))
+    plt.xlim(min(specwl),max(specwl))
+    plt.savefig(date_dir + "BART-bestFit-Spectrum.png")
+
+def plotabun(date_dir, atmfile, molfit):
+    '''
+    Plot abundance profiles from best fit run.
+
+    Input
+    -----
+    date_dir: string
+      Path to BART output directory
+    
+    atmfile: string
+      Name of best fit atmospheric file
+
+    molfit:  1D string array
+      Molecules to plot
+    '''
+
+    # Import best fit atmosphere results
+    species, pressure, temp, abundances = mat.readatm(date_dir + atmfile)
+
+    # Create array of indices for species to plot 
+    molfitindex = np.zeros(len(molfit), dtype='int')
+
+    k = 0
+
+    # Find the index of each species within the atmosphere file
+    for i in range(len(species)):
+        for j in range(len(molfit)):
+            if species[i] == molfit[j]:
+                molfitindex[k] = i
+                k += 1
+
+    plt.clf()
+
+    # Plot the abundance profile of each species
+    for i in range(len(molfit)):
+        plt.loglog(abundances[:,molfitindex[i]], pressure, label=species[molfitindex[i]], linewidth=4)
+
+    plt.legend(loc='upper left')
+    plt.xlabel('Molar Mixing Fraction')
+    plt.ylabel('Pressure (bars)')
+    plt.title('Best Fit Abundance Profiles')
+
+    plt.gca().invert_yaxis()
+    
+    plt.savefig(date_dir + 'abun_profiles.png')
+    
+                
+    
+
